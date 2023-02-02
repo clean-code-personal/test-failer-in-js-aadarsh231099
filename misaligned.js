69499c80a1fd5d603a22f8a7decd25904e81d8a9
Aadarsh <4nm19is001@nmamit.in>
2023-02-02 13:34:12 +0530

    added expected map
    added the expected mapping and hence compared the result with it to test for the error in pair number and color pair system.

--- misaligned.js
+++ misaligned.js
@@ -1,5 +1,4 @@
-const {expect} = require('chai')
-
+const {expect} = require('chai');
 function print_color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
@@ -11,6 +10,12 @@ function print_color_map() {
     return majorColors.length * minorColors.length;
 }
 
-result = print_color_map();
-expect(result).equals(25);
+const expected = [  '1 | White | Blue',  '2 | White | Orange',  '3 | White | Green',  '4 | White | Brown',  '5 | White | Slate',  '6 | Red | Blue',  '7 | Red | Orange',
+  '8 | Red | Green',  '9 | Red | Brown',  '10 | Red | Slate',  '11 | Black | Blue',  '12 | Black | Orange',  '13 | Black | Green',  '14 | Black | Brown',  '15 | Black | Slate',
+    '16 | Yellow | Blue',  '17 | Yellow | Orange',  '18 | Yellow | Green',  '19 | Yellow | Brown',  '20 | Yellow | Slate',  '21 | Violet | Blue',  '22 | Violet | Orange',
+      '23 | Violet | Green',  '24 | Violet | Brown',  '25 | Violet | Slate',];
+
+result=print_color_map();
+expect(print_color_map()).equals(25);
+expect(result).equals(expected);
 console.log('All is well (maybe!)');
