96c84bc7afda7ba2cdbb3f7c36100564f2e5e6be
Aadarsh <aadarshhanu23@gmail.com>
2023-02-06 14:40:01 +0530

    updated misaligned.js
    used an array to store the given output and compared it with the expected output for failure of test.

--- misaligned.js
+++ misaligned.js
@@ -10,12 +10,49 @@ function print_color_map() {
     return majorColors.length * minorColors.length;
 }
 
-const expected = [  '1 | White | Blue',  '2 | White | Orange',  '3 | White | Green',  '4 | White | Brown',  '5 | White | Slate',  '6 | Red | Blue',  '7 | Red | Orange',
-  '8 | Red | Green',  '9 | Red | Brown',  '10 | Red | Slate',  '11 | Black | Blue',  '12 | Black | Orange',  '13 | Black | Green',  '14 | Black | Brown',  '15 | Black | Slate',
-    '16 | Yellow | Blue',  '17 | Yellow | Orange',  '18 | Yellow | Green',  '19 | Yellow | Brown',  '20 | Yellow | Slate',  '21 | Violet | Blue',  '22 | Violet | Orange',
-      '23 | Violet | Green',  '24 | Violet | Brown',  '25 | Violet | Slate',];
-
 result=print_color_map();
 expect(result).equals(25);
-expect(result).equals(expected);
+
+let givenOutput = [];
+const out = console.log;
+
+console.log = (...args) => {
+  givenOutput.push(args.join(" "));
+};
+
+print_color_map();
+
+const expected = [  
+'1   | White   | Blue',  
+'2   | White   | Orange', 
+'3   | White   | Green',
+'4   | White   | Brown',  
+'5   | White   | Slate',  
+'6   | Red     | Blue',  
+'7   | Red     | Orange',
+'8   | Red     | Green',  
+'9   | Red     | Brown',  
+'10  | Red     | Slate',  
+'11  | Black   | Blue',  
+'12  | Black   | Orange',  
+'13  | Black   | Green',  
+'14  | Black   | Brown',  
+'15  | Black   | Slate',
+'16  | Yellow  | Blue',  
+'17  | Yellow  | Orange',  
+'18  | Yellow  | Green',  
+'19  | Yellow  | Brown',  
+'20  | Yellow  | Slate',  
+'21  | Violet  | Blue',  
+'22  | Violet  | Orange',
+'23  | Violet  | Green',  
+'24  | Violet  | Brown',  
+'25  | Violet  | Slate',
+];
+
+
+expect(givenOutput).equals(expected);
+console.log = out;
+
+
 console.log('All is well (maybe!)');
