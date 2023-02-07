57635d4a21ddeb7df22152e08a3bbff31d8d2e46
Aadarsh <aadarshhanu23@gmail.com>
2023-02-07 16:29:57 +0530

    used padEnd function in misaligned.js
    the padEnd is more easy and better approach for better alignment.

--- misaligned/color_map.js
+++ misaligned/color_map.js
@@ -14,8 +14,8 @@ function print_color_map() {
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
             let index = i * 5 + (j + 1);
-            let indexString = " ".repeat(maxIndexLength+1 - index.toString().length) + index;
-            let majorString = " ".repeat(maxMajorLength+1 - majorColors[i].length) + majorColors[i];
+            let indexString = index.toString().padEnd(maxIndexLength, " ");
+            let majorString = majorColors[i].padEnd(maxMajorLength, " ");
             map.push(`${indexString} | ${majorString} | ${minorColors[j]}`);
         }
     }
