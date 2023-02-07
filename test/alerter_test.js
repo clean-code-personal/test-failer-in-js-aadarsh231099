02ed57f556923e1c0e63eb17933b7b087f6adb7b
Aadarsh <aadarshhanu23@gmail.com>
2023-02-07 15:27:52 +0530

    Separated test files from function files
    made folders for respective problem files and kept only the test files in test dir for mocha

--- test/alerter_test.js
+++ test/alerter_test.js
@@ -1,9 +1,9 @@
 const {expect} = require('chai');
-let alertInCelcius = require('./alertInCel');
+let alertInCelcius = require('../alerter/alertInCel');
 let alertFailureCount = 0;
 
-describe('alertInCelcius ', function () {
-    it('should return no of alerts failed', function () {
+describe('tests for alertInCelcius: ', function () {
+    it('should return correct number of alerts failed', function () {
         alertFailureCount = alertInCelcius(400.5);
         alertFailureCount = alertInCelcius(303.6);
         console.log(`${alertFailureCount} alerts failed.`);
--- test/tshirts_test.js
+++ test/tshirts_test.js
@@ -1,17 +1,17 @@
 const {expect} = require('chai');
-const size=require('./tshirts');
+const size=require('../tshirts/tshirts_size');
 
-  describe('size', function () {
+  describe('tests for tshirt-size:', function () {
     it('should return "Enter positive value" when the value is less than 0', function () {
         expect(size(-2)).equals('Enter positive value');
     });
-    it('should return S when the value is 38', function () {
+    it('should return "S" when the value is 38', function () {
         expect(size(38)).equals('S');
     });
-    it('should return M when the value is 39', function () {
+    it('should return "M" when the value is 39', function () {
         expect(size(39)).equals('M');
     });
-    it('should return L when the value is 42', function () {
+    it('should return "L" when the value is 42', function () {
         expect(size(43)).equals('L');
     });
     it('should return "Value out of size limits" when the value is more than 50', function () {
